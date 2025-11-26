b177175eb50fcd9d66834935ead3d887599044fe
Jake Harris <jakec.harris@gmail.com>
2025-11-25 21:04:23 -0800

    Work on problem 2

--- 2024/day2/problem2.py
+++ 2024/day2/problem2.py
@@ -9,7 +9,6 @@ How many safe power reports are there in the provided data?
 '''
 
 import csv
-import re
 
 # Load report rows from input2.txt into one list
 reports = []
@@ -17,4 +16,27 @@ with open ('input2.txt', newline='') as file:
     report_reader = csv.reader(file, delimiter=' ')
     for row in report_reader:
         reports.append(row)
-print(reports[:5])
+
+
+
+
+rep = 0
+safe_count = 0
+while rep < len(reports[:2]):
+    for row in reports[:2]:
+        diffs = [int(row[i]) - int(row[i-1]) for i in range(len(row)-1, -1, -1)]
+        res = []
+        for dif in diffs:
+            if (dif >= 1) and (dif <= 3):
+                res.append(1)
+            elif (dif <= -1) and (dif >= -3):
+                res.append(0)
+            elif (dif == 0) or (abs(dif) > 3):
+                continue
+            
+        print(res)
+        if all(res) or not all(res):
+            safe_count += 1
+    rep += 1
+
+print(safe_count)
