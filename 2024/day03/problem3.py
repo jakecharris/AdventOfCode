e8076262572c1d03507145797ec9354d54da37fa
Jake Harris <jakec.harris@gmail.com>
2025-11-26 20:56:28 -0800

    Finish problem 3

--- 2024/day03/problem3.py
+++ 2024/day03/problem3.py
@@ -17,12 +17,40 @@ with open('input3.txt') as file:
 mul_lst = re.findall(r'(mul\(\d+,\d+\))', txt)
 
 # Replace unecessary 'mul()' text from mul_lst, multiply numbers, add products
-total_sum = 0
-for entry in mul_lst:
-    num_txt = re.sub(r'mul\(', '', entry)
-    num_txt = re.sub(r'\)', '', num_txt)
-    num_int = [int(num) for num in num_txt.split(',')]
-    product = num_int[0] * num_int[1]
-    total_sum += product
-print(total_sum)
+def total_product_sum(lst):
+    total_sum = 0
+    for entry in lst:
+        num_txt = re.sub(r'mul\(', '', entry)
+        num_txt = re.sub(r'\)', '', num_txt)
+        num_int = [int(num) for num in num_txt.split(',')]
+        product = num_int[0] * num_int[1]
+        total_sum += product
+    print(total_sum)
 
+total_product_sum(mul_lst)
+
+'''Problem 3.2: There are additional do() and don't() instructions throughout the
+text which enable/disable following mul() functions, respectively. Think of it
+like an on/off switch for multiplications. What is the new total sum of products 
+if you take only those mul(X,Y) which are after do() statements?
+'''
+
+# Run same idea from above, but look for 'do()' and 'don't()' in txt 
+sum_product = 0
+do_func = True
+for x in re.finditer(r'do\(\)|don\'t\(\)|(mul\(\d+,\d+\))', txt):
+    print("x[0]: ", x[0])
+    print("x[1]: ", x[1])
+    match x[0]: # x[0] will be for either "do()" or "dont()" 
+        case 'do()':
+            do_func = True
+        case 'don\'t()':
+            do_func = False
+        case _: # x[1] will be for either None (if do/dont) or a mul(X,Y)
+            if do_func:
+                num_txt = re.sub(r'mul\(', '', x[1])
+                num_txt = re.sub(r'\)', '', num_txt)
+                num_int = [int(num) for num in num_txt.split(',')]
+                product = num_int[0] * num_int[1]
+                sum_product += product
+print(sum_product)
