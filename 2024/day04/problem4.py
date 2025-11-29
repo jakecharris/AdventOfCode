6b51d2d32902311f4d3971592db31d84a975ad51
Jake Harris <jakec.harris@gmail.com>
2025-11-28 20:38:14 -0800

    Changes based on user's work

--- 2024/day04/problem4.py
+++ 2024/day04/problem4.py
@@ -4,16 +4,37 @@ You are given a wordsearch puzzle with the only word to find being "XMAS".
 However, it is written in multiple directions, including horizontally, vertically,
 diagonally, and backwards in all of the former directions. Some instances even 
 overlap each other. In the given puzzle, how many instances of "XMAS" are there?
+
+Solution adapted from 
+https://github.com/UserGeorge24/aoc_24_python/blob/main/day_04_task_01.py
 '''
 
-import numpy as np
+import re
 
-# Open text as numpy array
-arr = []
-with open('input4.txt') as file:
-    # arr += [line.strip() for line in file]
-    for line in file:
-        line = line.strip()
-        arr.append(list(line))
-    arr = np.asarray(arr, dtype=str)
-print(arr)
+# Open text array as list with each row as an index
+arr = [line.strip() for line in open('input4.txt').readlines()]
+
+xmas_sum = 0 # overall "XMAS" instances 
+pos_idx = 0 # position index
+
+# Function to count number of 'XMAS' regex in each direction of selected txt
+def count_xmas(txt):
+    global xmas_sum # allows for changing xmas_sum outside function
+    # regex to count number of forward + backward ([::-1]) txt instances
+    xmas_sum += len(re.findall('XMAS', txt)) + len(re.findall('XMAS', txt[::-1]))
+
+# Horizontal instances
+for i in arr:
+    count_xmas(arr)
+    
+# Vertical instances
+# make strings of each array column
+while True:
+    try:
+        col_txt = ''.join([col[pos_idx] for col in arr])
+    except:
+        break
+    # check for 'XMAS' in column text, move to new column
+    count_xmas(col_txt)
+    pos_idx += 1
+
