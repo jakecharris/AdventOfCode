425a428e52947b36642e1672a74fc56a18804577
Jake Harris <jakec.harris@gmail.com>
2025-12-03 13:39:17 -0800

    Solution from online

--- 2024/day04/problem4.py
+++ 2024/day04/problem4.py
@@ -132,3 +132,64 @@ print(xmas_sum)
     #     print(word[i:len(word)+i+1])
 word = 'abcd'
 print(word[:20])
+
+
+# solution from https://bversion.com/WordPress/2024/12/06/aoc-2024/#day-4
+# Part 1
+f = open("Day 4 - input.txt", "r")
+a = []
+w = ['']*8
+total = 0
+
+for i in f:
+    a.append(i.strip())
+
+s = 'Z' * (len(a[0])+6)
+for i in range(len(a)):
+    a[i] = 'ZZZ' + a[i] + 'ZZZ'
+    
+for i in range(3):
+    a.insert(0, s)
+    a.append(s)
+
+for y in range(len(a)):
+    for x in range(len(a[y])):
+        if a[y][x] == 'X':
+            w[0] = a[y][x:x+4]
+            w[1] = a[y][x]+a[y+1][x+1]+a[y+2][x+2]+a[y+3][x+3]
+            w[2] = a[y][x]+a[y+1][x]+a[y+2][x]+a[y+3][x]
+            w[3] = a[y][x]+a[y+1][x-1]+a[y+2][x-2]+a[y+3][x-3]
+            w[4] = a[y][x]+a[y][x-1]+a[y][x-2]+a[y][x-3]
+            w[5] = a[y][x]+a[y-1][x-1]+a[y-2][x-2]+a[y-3][x-3]
+            w[6] = a[y][x]+a[y-1][x]+a[y-2][x]+a[y-3][x]
+            w[7] = a[y][x]+a[y-1][x+1]+a[y-2][x+2]+a[y-3][x+3]
+            total += w.count('XMAS')    
+            
+print(total)
+
+# part 2
+f = open("Day 4 - input.txt", "r")
+a = []
+w = ['']*2
+total = 0
+
+for i in f:
+    a.append(i.strip())
+
+s = 'Z' * (len(a[0])+2)
+for i in range(len(a)):
+    a[i] = 'Z' + a[i] + 'Z'
+    
+for i in range(3):
+    a.insert(0, s)
+    a.append(s)
+
+for y in range(len(a)):
+    for x in range(len(a[y])):
+        if a[y][x] == 'A':
+            w[0] = a[y-1][x-1]+a[y][x]+a[y+1][x+1]
+            w[1] = a[y+1][x-1]+a[y][x]+a[y-1][x+1]
+            if w.count('MAS')+w.count('SAM') == 2:
+                total += 1
+                       
+print(total)
