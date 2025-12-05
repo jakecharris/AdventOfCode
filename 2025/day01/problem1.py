3ad24408a3fde0e05bb5ba0033c0fdb7887b5371
Jake Harris <jakec.harris@gmail.com>
2025-12-05 13:15:48 -0800

    Initial 2025 commit

---
+++ 2025/day01/problem1.py
@@ -0,0 +1,10 @@
+'''
+Day 1: Secret Entrance (https://adventofcode.com/2025/day/1)
+
+Problem 1.1: You need to get into the base, but there is a set of instrucitons
+to open a safe with numbers 0-99. The instructions tell you which direction to
+turn (Right or Left) and by how many units. The initial start is at 50.
+
+How many times while turning based on the instructions do you pass 0?
+'''
+
