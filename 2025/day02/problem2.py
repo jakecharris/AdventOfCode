af0d92c2c4a1a6db9716950708273827a7854338
Jake Harris <jakec.harris@gmail.com>
2025-12-07 17:30:10 -0800

    Initial commit for problem 2

---
+++ 2025/day02/problem2.py
@@ -0,0 +1,12 @@
+'''
+Day 2: Gift Shop (https://adventofcode.com/2025/day/2)
+
+There are a series of product ID ranges. Invalid IDs are those with made of
+just two sequences of repeating digits (e.g. 55 (two 5s), 1010 (two 10s), 123123
+(two 123s)). None of the numbers have leading zeroes.
+
+Problem 2.1: From an input of given IDs, find all the invalid IDs and sum all
+them up. 
+'''
+
+
