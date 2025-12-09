46540fc1ca526ad89d1f42ce2f601b7044f6b4c8
Jake Harris <jakec.harris@gmail.com>
2025-12-08 17:52:36 -0800

    Progress on problem 3.2

--- 2025/day03/problem3.py
+++ 2025/day03/problem3.py
@@ -20,12 +20,40 @@ joltages = [line.strip() for line in open('input3.txt').readlines()]
 # Solution based on https://github.com/mgtezak/Advent_of_Code/blob/master/2025/03/p1.py
 def part1():
     total_output = 0
-    for jolt in joltages[:5]:
+    for jolt in joltages:
         n1 = max(jolt[:-1])
         n2 = max(jolt[jolt.find(n1)+1:])
-        print(n1, n2)
         total_output += int(n1 + n2)
     print(f'Answer part 1: {total_output}')
 
 part1()  # Answer: 17524
 
+'''
+Problem 3.2: Now, you want to increase the total output of the battery banks by
+turning on 12 of the highest joltage batteries from left to right. What is the
+new total joltage output?
+'''
+
+# every len(joltage) = 100, so need to drop 88 digits
+def part2():
+    total_output = 0
+    for jolt in joltages:
+        bat_bank = ''
+        pos = 0
+        for i in range(12, 0, -1):
+            digit = max(jolt[pos:-i])
+            pos = jolt.find(digit, pos) + 1
+            # print(f'i {i}, Digit {digit}, Pos {pos}')
+            bat_bank += digit
+        
+        total_output += int(bat_bank)
+    
+    print(f'Answer part 2: {total_output}')
+
+# part2() # Answer: 
+
+print([i for i in range(11, 0, -1)])
+print([i for i in reversed(range(12))])
+
+n = '123'   
+print(n[2:-1])
