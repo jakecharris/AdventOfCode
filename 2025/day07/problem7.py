1f6bc13279f84a9188a0a83a4e26945fd0b0760f
Jake Harris <jakec.harris@gmail.com>
2025-12-24 19:01:25 -0800

    Progress on problem 7.1

--- 2025/day07/problem7.py
+++ 2025/day07/problem7.py
@@ -25,7 +25,23 @@ be split based on the provided diagram?
 ..|.|.|||.|.|..
 .|^|||^||.||^|.
 .|.|||.||.||.|.    pattern: only count when a '|' is above a '^'???
-|^|^|^|^|^|||^|
+|^|^|^|^|^|||^|             only count when a 
 |.|.|.|.|.|||.|
 
 '''
+
+# load in unmod file
+# for row 0 has S
+# for odd rows, change periods to | to reflect beam pattern
+    # if beam right above ^ , then change periods next to it to beams, count split
+    # change periods in rows below to | 
+
+# open lines of array file 
+array = [line for line in open('input7.txt').read().strip().splitlines()] 
+even_rows = [array[i] for i in range(len(array)) if i % 2 == 0]
+odd_rows = [array[i] for i in range(len(array)) if i % 2 != 0]
+
+print(all('^' in even_rows[i] for i in range(2, len(even_rows))))  # True
+print(any('^' in odd_rows[i] for i in range(len(odd_rows))))  # False
+
+print(even_rows[0].index('S'))  # S in row 0 at index 70
