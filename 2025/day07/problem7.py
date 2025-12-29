c9edabbc0bcbd8b9f326a04ce7e27cefe5a7579d
Jake Harris <jakec.harris@gmail.com>
2025-12-29 15:44:58 -0600

    Progress on problem 7.1

--- 2025/day07/problem7.py
+++ 2025/day07/problem7.py
@@ -29,6 +29,7 @@ be split based on the provided diagram?
 |.|.|.|.|.|||.|
 
 '''
+import re
 
 # load in unmod file
 # for row 0 has S
@@ -44,4 +45,42 @@ odd_rows = [array[i] for i in range(len(array)) if i % 2 != 0]
 print(all('^' in even_rows[i] for i in range(2, len(even_rows))))  # True
 print(any('^' in odd_rows[i] for i in range(len(odd_rows))))  # False
 
-print(even_rows[0].index('S'))  # S in row 0 at index 70
+print(even_rows[0].index('.'))  # S in row 0 at index 70
+
+# find indexes of chars using enumerate in a dict
+
+total_splits = 0
+i = 1
+for i in range(len(array)):
+    # dict of indexes of symbols in row
+    char_idx = {ele: [] for ele in array[i]}
+    for idx, ele in enumerate(array[i]):
+        char_idx[ele].append(idx)
+    
+    for row in array:
+        # first beam symbol below 'S'
+        if ('S' in row[i]):
+            row_lst = list(array[i+1])
+            for j in char_idx['S']:
+                row_lst[j] = '|' 
+            ''.join(row_lst)
+        # split beams on either side of splitter
+        if ('^' in row[i]) and ('|' in row[i-1]):
+            
+            beam_lst = list(array[i-1])
+            split_lst = list(array[i])
+            for j in char_idx['|']:
+                split_lst[j-1] = '|' 
+                split_lst[j+1] = '|'
+            ''.join(row_lst)
+            
+            
+        else:
+            pass
+    
+
+# change '.' in row 1, index 70 below S to '|'  
+row1_lst = list(array[1])
+row1_lst[70] = '|'
+''.join(row1_lst)
+
